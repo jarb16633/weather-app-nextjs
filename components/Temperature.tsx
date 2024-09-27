5e51f999c9471f695cab37068b8f3e4be7a9c8ea
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2024-09-28 01:00:54 +0700

    implement kelvinToCelsius calculation to Temperature component

--- components/Temperature.tsx
+++ components/Temperature.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useGlobalContext } from "@/app/context/globalContext";
-import React from "react";
+import { kelvinToCelsius } from "@/utils/misc";
+import React, { useState } from "react";
 
 type Props = {};
 
@@ -8,11 +9,21 @@ const Temperature = (props: Props) => {
   const { forecast } = useGlobalContext();
   const { main, timezone, name, weather } = forecast;
 
-  console.log(name);
-
   if (!forecast || !weather) {
     return <div>Loading...</div>;
   }
+
+  console.log(kelvinToCelsius(main.temp));
+
+  const temp = kelvinToCelsius(main?.temp);
+  const mintemp = kelvinToCelsius(main?.temp_min);
+  const maxTemp = kelvinToCelsius(main?.temp_max);
+
+  // State
+  const [localTime, setLocalTime] = useState<string>("");
+  const [currentDay, setCurrentDay] = useState<string>("");
+
+  console.log(weather[0]);
 
   return (
     <div className="pt-6 pb-5 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none">
