5c1ee777dfb728820bf3872d9f442bb697ce1276
jarb16633 <sahassavas.rim@gmail.com>
2024-09-30 11:30:16 +0700

    update getIcon code to return URL dynamically base on the response

--- components/Temperature.tsx
+++ components/Temperature.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useGlobalContext } from "@/app/context/globalContext";
+import { clearSky, cloudy, drizzleIcon, rain, snow } from "@/utils/Icons";
 import { kelvinToCelsius } from "@/utils/misc";
 import React, { useState } from "react";
 
@@ -23,11 +24,19 @@ const Temperature = (props: Props) => {
   const [localTime, setLocalTime] = useState<string>("");
   const [currentDay, setCurrentDay] = useState<string>("");
 
-  console.log(weather[0]);
+  const { main: weatherMain, description } = weather[0];
+
+  const getIcon = () => {
+    const iconCode = weather[0].icon;
+    return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  };
 
   return (
     <div className="pt-6 pb-5 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none">
-      Temperature
+      <p className="flex justify-between items-center">
+        <span className="font-medium">{currentDay}</span>
+        <span className="font-medium">{localTime}</span>
+      </p>
     </div>
   );
 };
