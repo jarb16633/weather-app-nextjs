86e4be8800815e24cf61ff57133c713c85fb0c3f
jarb16633 <sahassavas.rim@gmail.com>
2024-09-30 22:43:48 +0700

    update layout in page

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ export default function Home() {
   return (
     <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
       <Navbar />
-      <div className="pb-4 flex flex-col gap-4 md:flex-row">
-        <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
+      <div className="pb-4 flex flex-row gap-4">
+        <div className="w-[37.5%]">
           <Temperature />
         </div>
-        <div className="flex flex-col w-full">
-          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="w-[62.5%] grid grid-rows-2 gap-4">
+          <div className="row-span-1">
+            <AirPollution />
+          </div>
+          <div className="row-span-1">
             <AirPollution />
           </div>
         </div>
