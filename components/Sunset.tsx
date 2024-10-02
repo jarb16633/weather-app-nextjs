56d4b1b913428c9a855eb48e008c4a0ee445520a
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2024-10-02 17:32:40 +0700

    update sunrise sunset component

--- components/Sunset.tsx
+++ components/Sunset.tsx
@@ -4,8 +4,7 @@ import { useGlobalContext } from "@/app/context/globalContext";
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
 import { unixToTime } from "@/utils/misc";
-import { sunset } from "@/utils/Icons";
-import { Progress } from "./ui/progress";
+import { sunrise, sunset } from "@/utils/Icons";
 
 type Props = {};
 
@@ -23,13 +22,24 @@ const Sunset = (props: Props) => {
   const sunriseTime = unixToTime(forecast?.sys?.sunrise, timezone);
 
   return (
-    <div className="h-full pt-2 px-4 border rounded-xl flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none">
-      <div className="flex items-center gap-2 font-medium text-base sm:text-lg md:text-xl">
-        <h2>{sunset} Sunset</h2>
-      </div>
-      <div className="pb-4">
-        <p className="text-2xl font-bold">{sunsetTime}</p>
-        <p className="text-sm sm:text-base">Sunrise: {sunriseTime}</p>
+    <div className="h-full min-w-[150px] px-4 border rounded-xl flex justify-evenly items-center dark:bg-dark-grey shadow-sm dark:shadow-none">
+      <div className="grid grid-cols-2 gap-10">
+        <div className="col-span-1">
+          <h2 className="flex flex-row items-center text-center gap-2 font-medium text-base sm:text-lg md:text-xl">
+            {sunset} Sunset
+          </h2>
+          <p className="text-2xl font-bold items-center text-center">
+            {sunsetTime}
+          </p>
+        </div>
+        <div className="col-span-1">
+          <h2 className="flex flex-row items-center text-center gap-2 font-medium text-base sm:text-lg md:text-xl">
+            {sunrise} Sunrise
+          </h2>
+          <p className="text-2xl font-bold items-center text-center">
+            {sunriseTime}
+          </p>
+        </div>
       </div>
     </div>
   );
--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -18,6 +18,7 @@ import {
   UsersRound,
   CalendarDays,
   SunDim,
+  Sunrise,
 } from "lucide-react";
 
 export const commandIcon = <Command size={14} />;
@@ -30,7 +31,7 @@ export const clearSky = <CloudSun size={30} />;
 export const cloudy = <Cloudy size={30} />;
 export const navigation = <Navigation size={15} />;
 export const thermo = <ThermometerSun size={15} />;
-export const sunset = <Sunset size={15} />;
+export const sunset = <Sunset size={20} />;
 export const wind = <Wind size={15} />;
 export const gauge = <Gauge size={15} />;
 export const droplets = <Droplets size={15} />;
@@ -39,3 +40,4 @@ export const eye = <Eye size={15} />;
 export const people = <UsersRound size={15} />;
 export const calender = <CalendarDays size={15} />;
 export const sun = <SunDim size={15} />;
+export const sunrise = <Sunrise size={20} />;
