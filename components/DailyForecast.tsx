e3c081e76bdd927a95dddd463aa33a684edfe095
jarb16633 <sahassavas.rim@gmail.com>
2024-10-07 16:50:43 +0700

    update dailyForecast component to display time and temp

--- components/DailyForecast.tsx
+++ components/DailyForecast.tsx
@@ -2,6 +2,9 @@
 import { useGlobalContext } from "@/app/context/globalContext";
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import moment from "moment";
+import { kelvinToCelsius } from "@/utils/misc";
 
 type Props = {};
 
@@ -11,14 +14,61 @@ const DailyForecast = (props: Props) => {
   const { weather } = forecast;
   const { city, list } = fiveDayForecast;
 
+  const temp = kelvinToCelsius(forecast.main.temp);
+
   if (!fiveDayForecast || !city || !list)
     return <Skeleton className="h-[12rem] w-full" />;
 
   if (!forecast || !weather) return <Skeleton className="h-[12rem] w-full" />;
 
+  const today = new Date();
+  const todayString = today.toISOString().split("T")[0];
+
+  //filter list for today's forecast
+  const todayForecast = list.filter(
+    (forecast: { dt_txt: string; main: { temp: number } }) => {
+      return forecast.dt_txt.startsWith(todayString);
+    }
+  );
+
+  // console.log(todayForecast);
+
+  const { main: weatherMain } = weather[0];
+
+  const getIcon = () => {
+    const iconCode = weatherMain.icon;
+    return `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+  };
+
   return (
     <div className="h-full pt-2 px-4 gap-2 border rounded-xl flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none">
-      DailyForecast
+      <div className="h-full flex gap-10 overflow-hidden">
+        {todayForecast.length < 1 ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="w-full">
+            <Carousel>
+              <CarouselContent>
+                {todayForecast.map(
+                  (forecast: { dt_txt: string; main: { temp: number } }) => {
+                    return (
+                      <CarouselItem
+                        key={forecast.dt_txt}
+                        className="flex flex-col gap-4 cursor-grab"
+                      >
+                        <p className="text-gray-300">
+                          {moment(forecast.dt_txt).format("HH:mm")}
+                        </p>
+                        {temp}°
+                      </CarouselItem>
+                    );
+                  }
+                )}
+              </CarouselContent>
+            </Carousel>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- components/Sunset.tsx
+++ components/Sunset.tsx
@@ -25,13 +25,13 @@ const Sunset = (props: Props) => {
     <div className="h-full min-w-[150px] px-4 border rounded-xl flex justify-evenly items-center dark:bg-dark-grey shadow-sm dark:shadow-none">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-4 w-full">
         <div className="col-span-1">
-          <h2 className="flex flex-row text-center items-center text-xs justify-center gap-2 font-medium sm:justify-center sm:text-center md:text-lg">
+          <h2 className="flex flex-row text-center items-center text-xs justify-center gap-2 font-medium sm:justify-center sm:text-center md:text-base">
             {sunset} Sunset
           </h2>
           <p className="text-2xl font-bold text-center">{sunsetTime}</p>
         </div>
         <div className="col-span-1">
-          <h2 className="flex flex-row text-center items-center text-xs justify-center gap-2 font-medium sm:justify-center sm:text-center md:text-lg">
+          <h2 className="flex flex-row text-center items-center text-xs justify-center gap-2 font-medium sm:justify-center sm:text-center md:text-base">
             {sunrise} Sunrise
           </h2>
           <p className="text-2xl font-bold text-center">{sunriseTime}</p>
